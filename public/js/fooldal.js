3c96f5e450b792775e0a107909f9726613482377
Takács Marcell <90751998+TakacsMarcell@users.noreply.github.com>
2022-01-12 09:05:11 +0100

    fooldal
    #2

---
+++ public/js/fooldal.js
@@ -0,0 +1,42 @@
+const app = Vue.createApp({
+  data() {
+      return {
+          valasztott:  '',
+          selected: '',
+      };
+  },
+  methods: {
+    selectionChanged() {
+      console.log(selected.value);
+    }
+  },
+  watch: {
+      selected(value)
+      {
+        console.log(value);
+        if(value == "R")
+        {
+          this.valasztott = "R";
+        }
+        if(value == "A")
+        {
+          this.valasztott = "A";
+        }
+      }
+  }
+});
+app.mount('#cars')
+
+var target = $('#target'),
+    val = target.val();
+
+function monitor()
+{
+    var current_val = $(this).val();
+    if (current_val != val) {
+        console.log('changed from', val, 'to', current_val);
+        val = current_val;
+    }
+}
+
+target.keypress(monitor);
