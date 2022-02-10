3db8636a8c7e327f9a17a3390c65e474c2d55fb7
Pusoma Gergő <pusoma.gergo@jedlik.helyi>
2022-02-10 09:22:01 +0100

    Adatbazis uj adatokkal, bug fixes
    #3

--- CarScopeDatabase.sql
+++ CarScopeDatabase.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2022. Jan 12. 20:48
+-- Létrehozás ideje: 2022. Feb 10. 09:20
 -- Kiszolgáló verziója: 10.4.21-MariaDB
--- PHP verzió: 7.3.31
+-- PHP verzió: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -77,8 +77,9 @@ INSERT INTO `felhasznalo` (`FID`, `Felhasználónév`, `Jelszó`, `Email`) VALUE
 (4, 'Dromarci', 'Virág4ever', 'takacsmarci@gmail.com'),
 (5, 'DaweDie', 'TaKa', 'dawedie@gmail.com'),
 (6, 'asdasd', 'c329936d9323991c7e6039f3222082', 'asdasd@asdasd.asd'),
-(8, 'Admin', '6a6bc131fe8ea7acd0a9caa1771be2a8', 'admin@admin.admin'),
-(9, 'Gregoryy022', '2ac5efd7077cb66d640029a6862eb8d9', 'gregoryy@gmail.com');
+(9, 'Gregoryy022', '2ac5efd7077cb66d640029a6862eb8d9', 'gregoryy@gmail.com'),
+(11, 'Admin1122', '9695476e054dcb63b752dc141702ff50', 'admin@admin.com'),
+(12, 'Admin112233', 'f7c6eeeb40882248ed771565f180382d', 'admin@admin.admin');
 
 -- --------------------------------------------------------
 
@@ -88,20 +89,28 @@ INSERT INTO `felhasznalo` (`FID`, `Felhasználónév`, `Jelszó`, `Email`) VALUE
 
 CREATE TABLE `gumiabroncs` (
   `GID` int(11) NOT NULL,
-  `Gyarto` varchar(30) COLLATE utf8_hungarian_ci NOT NULL,
+  `Gyarto` varchar(150) COLLATE utf8_hungarian_ci NOT NULL,
   `Evszak` set('Nyári','Téli','Négyévszakos','') COLLATE utf8_hungarian_ci NOT NULL,
   `Kategoria` decimal(2,0) NOT NULL,
   `Ar` int(6) NOT NULL,
-  `Info_ID` int(11) NOT NULL
+  `Atmero` int(11) NOT NULL,
+  `Oldalfal` int(11) NOT NULL,
+  `Szelesseg` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 --
 -- A tábla adatainak kiíratása `gumiabroncs`
 --
 
-INSERT INTO `gumiabroncs` (`GID`, `Gyarto`, `Evszak`, `Kategoria`, `Ar`, `Info_ID`) VALUES
-(2, 'Michelin', 'Nyári', '7', 29000, 1),
-(3, 'Hankook', 'Téli', '6', 25000, 2);
+INSERT INTO `gumiabroncs` (`GID`, `Gyarto`, `Evszak`, `Kategoria`, `Ar`, `Atmero`, `Oldalfal`, `Szelesseg`) VALUES
+(2, 'Michelin Pilot Sport', 'Nyári', '7', 29000, 15, 50, 185),
+(3, 'Hankook Prime', 'Téli', '6', 25000, 16, 40, 210),
+(10, 'Michelin Alpin 6', 'Téli', '9', 41000, 16, 50, 225),
+(11, 'Michelin Pilot Sport 4 S', 'Nyári', '10', 54000, 17, 35, 285),
+(12, 'Michelin Pilot Sport', 'Nyári', '9', 48000, 19, 45, 265),
+(13, 'Toyo Proxess Sport 1', 'Nyári', '7', 38500, 18, 40, 225),
+(14, 'Continental Cross Contact', 'Téli', '6', 29000, 16, 60, 195),
+(15, 'Pirelli Scorpion A/T Plus', 'Négyévszakos', '5', 34990, 18, 55, 215);
 
 -- --------------------------------------------------------
 
@@ -112,7 +121,7 @@ INSERT INTO `gumiabroncs` (`GID`, `Gyarto`, `Evszak`, `Kategoria`, `Ar`, `Info_I
 CREATE TABLE `info` (
   `IID` int(11) NOT NULL,
   `Rendszam` varchar(10) COLLATE utf8_hungarian_ci DEFAULT NULL,
-  `Alvazszam` varchar(30) COLLATE utf8_hungarian_ci NOT NULL,
+  `Alvazszam` varchar(17) COLLATE utf8_hungarian_ci NOT NULL,
   `Futottkm` int(11) NOT NULL,
   `Evjarat` year(4) NOT NULL,
   `Allapot` set('Újszerű','Használt','Enyhén sérült','Frissen felújított','Sérült') COLLATE utf8_hungarian_ci NOT NULL,
@@ -120,16 +129,18 @@ CREATE TABLE `info` (
   `Okmanyok` set('Érvényes magyar okmányokkal','Lejárt magyar okmányokkal','Külföldi okmányokkal','Okmányok nélkül') COLLATE utf8_hungarian_ci NOT NULL,
   `Muszakierv` date NOT NULL,
   `Gumiabroncs` set('Nyári','Téli','Négyévszakos','') COLLATE utf8_hungarian_ci NOT NULL,
-  `Auto_AID` int(11) NOT NULL
+  `Auto_AID` int(11) NOT NULL,
+  `Kepcim` text COLLATE utf8_hungarian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 --
 -- A tábla adatainak kiíratása `info`
 --
 
-INSERT INTO `info` (`IID`, `Rendszam`, `Alvazszam`, `Futottkm`, `Evjarat`, `Allapot`, `VezetettSzervK`, `Okmanyok`, `Muszakierv`, `Gumiabroncs`, `Auto_AID`) VALUES
-(1, 'RPL-916', 'FZfrrg248467', 35000, 2019, 'Újszerű', 1, 'Érvényes magyar okmányokkal', '2022-09-10', 'Téli', 1),
-(2, 'SGA-030', 'FZfrefs4748467', 145000, 2005, 'Használt', 0, 'Érvényes magyar okmányokkal', '2023-03-10', 'Nyári', 14);
+INSERT INTO `info` (`IID`, `Rendszam`, `Alvazszam`, `Futottkm`, `Evjarat`, `Allapot`, `VezetettSzervK`, `Okmanyok`, `Muszakierv`, `Gumiabroncs`, `Auto_AID`, `Kepcim`) VALUES
+(1, 'RPL-916', 'PRB12345678901234', 35000, 2019, 'Újszerű', 1, 'Érvényes magyar okmányokkal', '2022-09-10', 'Téli', 1, 'https://th.bing.com/th/id/OIP.mfGojkzSsAvX-qOiXZy9nwHaEK?pid=ImgDet&rs=1'),
+(2, 'SGA-030', 'FZ123456789123456', 145000, 2005, 'Használt', 0, 'Érvényes magyar okmányokkal', '2023-03-10', 'Nyári', 14, 'https://i3.infocar.ua/img/bazar2/733/732349/5581219_3.jpg'),
+(3, 'HKR-115', '2147grgrf47845741', 451200, 2001, 'Használt', 0, 'Érvényes magyar okmányokkal', '2023-02-09', 'Négyévszakos', 5, 'https://auto-database.com/image/opel-corsa-c-2001-pictures-116229.jpg');
 
 -- --------------------------------------------------------
 
@@ -163,8 +174,7 @@ ALTER TABLE `felhasznalo`
 -- A tábla indexei `gumiabroncs`
 --
 ALTER TABLE `gumiabroncs`
-  ADD PRIMARY KEY (`GID`),
-  ADD UNIQUE KEY `Info_ID` (`Info_ID`);
+  ADD PRIMARY KEY (`GID`);
 
 --
 -- A tábla indexei `info`
@@ -194,29 +204,23 @@ ALTER TABLE `auto`
 -- AUTO_INCREMENT a táblához `felhasznalo`
 --
 ALTER TABLE `felhasznalo`
-  MODIFY `FID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `FID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT a táblához `gumiabroncs`
 --
 ALTER TABLE `gumiabroncs`
-  MODIFY `GID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `GID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT a táblához `info`
 --
 ALTER TABLE `info`
-  MODIFY `IID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `IID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Megkötések a kiírt táblákhoz
 --
-
---
--- Megkötések a táblához `gumiabroncs`
---
-ALTER TABLE `gumiabroncs`
-  ADD CONSTRAINT `Gumiabroncs_Info` FOREIGN KEY (`Info_ID`) REFERENCES `info` (`IID`) ON DELETE NO ACTION;
 
 --
 -- Megkötések a táblához `info`
