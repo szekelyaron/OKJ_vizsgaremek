0390ddbf31af0272b638be8737624d76852acebd
Pusoma Gergő <pusomagergo12@gmail.com>
2022-02-22 08:56:33 +0100

    termekek kilistázása méret/évszak alapján

--- public/js/termekek.js
+++ public/js/termekek.js
@@ -2,4 +2,23 @@ function timer(obj){
     setTimeout(function() {
         $('#exampleModal').modal('hide');
     }, 800);
-}
+};
+
+const app = Vue.createApp({
+    data() {
+        return {
+            valasztottEvszak:  '',
+            valasztottMeret:  '',
+        };
+    },
+    methods: {
+      selectionChanged() {
+      }
+    },
+    
+  });
+  app.mount('#termekek')
+  
+  var target = $('#target'),
+      val = target.val();
+  
