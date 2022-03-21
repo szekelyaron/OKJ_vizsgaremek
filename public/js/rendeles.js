e064f2998d240a1f8e8ed926e3d12956a8e6a3d7
Pusoma Gergő <pusomagergo12@gmail.com>
2022-03-21 09:05:09 +0100

    vue ellenőrzés és pattern

--- public/js/rendeles.js
+++ public/js/rendeles.js
@@ -51,14 +51,14 @@ const app = Vue.createApp({
     },
 
     street(value) {
-      if (value.length > 1 && value.replace(' ','') != '') {
+      if (value.length > 1 && /^[qwertzuiopőúöüóasdfghjkléáűíyxcvbnmQWERTZUIOPŐÚÖÜÓASDFGHJKLÉÁŰÍYXCVBNM]+$/.test(value) == true && value.replace(' ','') != '') {
         this.utca_validate = true;
       } else {
         this.utca_validate = false;
       }
     },
     city(value) {
-      if (value.length > 2 && /^[a-zA-Z]+$/.test(value) == true && value.replace(' ','') != '') {
+      if (value.length > 1 && /^[qwertzuiopőúöüóasdfghjkléáűíyxcvbnmQWERTZUIOPŐÚÖÜÓASDFGHJKLÉÁŰÍYXCVBNM]+$/.test(value) == true && value.replace(' ','') != '') {
         this.varos_validate = true;
       } else {
         this.varos_validate = false;
@@ -85,7 +85,7 @@ const app = Vue.createApp({
     vezetek(value){
       let a = value.replace(' ',''); 
       console.log(a);
-      if(/^[a-zA-Z]+$/.test(a) == true && a.length > 0)
+      if(/^[qwertzuiopőúöüóasdfghjkléáűíyxcvbnmQWERTZUIOPŐÚÖÜÓASDFGHJKLÉÁŰÍYXCVBNM]+$/.test(a) == true && a.length > 0)
       {
         this.vezetek_validate = true;
       }
@@ -96,7 +96,7 @@ const app = Vue.createApp({
     kereszt(value){
       let b = value.replace(' ',''); 
       console.log(b);
-      if(/^[a-zA-Z]+$/.test(b) == true && b.length > 0)
+      if(/^[qwertzuiopőúöüóasdfghjkléáűíyxcvbnmQWERTZUIOPŐÚÖÜÓASDFGHJKLÉÁŰÍYXCVBNM]+$/.test(b) == true && b.length > 0)
       {
         this.kereszt_validate = true;
       }
