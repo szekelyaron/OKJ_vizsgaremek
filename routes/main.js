85c5818fc45389d84d6a5e0a8c2ff31b48dbf7cd
Pusoma Gergő <pusomagergo12@gmail.com>
2022-04-12 21:46:06 +0200

    teszt és formazas

--- routes/main.js
+++ routes/main.js
@@ -19,53 +19,31 @@ router.get('/', mainController.getIndex);
 router.get('/', mainController.checkLogin);
 
 router.get('/bejelentkezes', mainController.getBejelentkezes);
-
 router.post('/bejelentkezes', mainController.postLogin);
-
 router.get('/bejelentkezes', mainController.checkLogin);
-
 router.get('/regisztracio', mainController.getRegisztracio);
-
 router.post('/regisztracio', validationForm.form ,mainController.validateRegistration);
-
 router.get('/sugo', mainController.getSugo);
-
 router.get('/kapcsolat', mainController.getContactus);
-
 router.post('/contactuskuld', mainController.postContactus);
 
 // Fooldal
 
 router.get('/fooldal', mainController.getFooldal);
-
 router.post('/fooldal',mainController.rendszamlekerdezes);
-
 router.post('/fooldal',mainController.checkLogin);
-
 router.post('/kijelentkezes', mainController.postLogout);
-
 router.get('/vendeg',mainController.getVendeg);
-
 router.post('/vendeg',mainController.rendszamlekerdezesVendeg);
-
 router.get('/termekek',mainController.gumikLekerdezes);
-
 router.get('/kosar',mainController.getKosar);
-
 router.post('/kosarhozad',mainController.gumiKosarba);
-
 router.post('/kosarhozadSzett',mainController.gumiKosarbaSzett);
-
 router.post('/kosarMinusz', mainController.gumiKosartorol);
-
 router.post('/kosarPlusz', mainController.gumiKosarPlusz);
-
 router.post('/elkuld',mainController.postRendeles);
-
 router.get('/kalkulator', mainController.getKalkulator);
-
 router.post('/kalkulator', mainController.postKalkulator);
-
 router.post('/torol', mainController.gumiKosartorolTeljes);
 
 module.exports = router;
